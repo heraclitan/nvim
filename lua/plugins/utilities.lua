db2d4630d169666ef62ba772a382486469e48238
Paul Beck <sparkfox23@mailbox.org>
2025-03-17 17:43:20 -0400

    utilities.lua fix

--- lua/plugins/utilities.lua
+++ lua/plugins/utilities.lua
@@ -36,4 +36,171 @@ return {
         signs = {
           add = { text = "▎" },
           change = { text = "▎" },
-          delete
+          delete = { text = "▁" },
+          topdelete = { text = "▔" },
+          changedelete = { text = "▎" },
+          untracked = { text = "▎" },
+        },
+        signcolumn = true,
+        numhl = false,
+        linehl = false,
+        word_diff = false,
+        watch_gitdir = {
+          interval = 1000,
+          follow_files = true,
+        },
+        attach_to_untracked = true,
+        current_line_blame = false,
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = "eol",
+          delay = 1000,
+          ignore_whitespace = false,
+        },
+        current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+        sign_priority = 6,
+        update_debounce = 100,
+        status_formatter = nil,
+        max_file_length = 40000,
+        preview_config = {
+          border = "rounded",
+          style = "minimal",
+          relative = "cursor",
+          row = 0,
+          col = 1,
+        },
+        yadm = {
+          enable = false,
+        },
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+          
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+          
+          -- Navigation
+          map("n", "]c", function()
+            if vim.wo.diff then
+              return "]c"
+            end
+            vim.schedule(function()
+              gs.next_hunk()
+            end)
+            return "<Ignore>"
+          end, { expr = true })
+          
+          map("n", "[c", function()
+            if vim.wo.diff then
+              return "[c"
+            end
+            vim.schedule(function()
+              gs.prev_hunk()
+            end)
+            return "<Ignore>"
+          end, { expr = true })
+          
+          -- Actions
+          map("n", "<leader>gs", gs.stage_hunk)
+          map("n", "<leader>gr", gs.reset_hunk)
+          map("v", "<leader>gs", function()
+            gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+          end)
+          map("v", "<leader>gr", function()
+            gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+          end)
+          map("n", "<leader>gS", gs.stage_buffer)
+          map("n", "<leader>gu", gs.undo_stage_hunk)
+          map("n", "<leader>gR", gs.reset_buffer)
+          map("n", "<leader>gp", gs.preview_hunk)
+          map("n", "<leader>gb", function()
+            gs.blame_line({ full = true })
+          end)
+          map("n", "<leader>gtb", gs.toggle_current_line_blame)
+          map("n", "<leader>gd", gs.diffthis)
+          map("n", "<leader>gD", function()
+            gs.diffthis("~")
+          end)
+          map("n", "<leader>gtd", gs.toggle_deleted)
+        end,
+      })
+    end,
+  },
+  
+  -- Surround operations
+  {
+    "kylechui/nvim-surround",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-surround").setup({
+        keymaps = {
+          insert = "<C-g>s",
+          insert_line = "<C-g>S",
+          normal = "ys",
+          normal_cur = "yss",
+          normal_line = "yS",
+          normal_cur_line = "ySS",
+          visual = "S",
+          visual_line = "gS",
+          delete = "ds",
+          change = "cs",
+        },
+      })
+    end,
+  },
+  
+  -- Terminal integration
+  {
+    "akinsho/toggleterm.nvim",
+    keys = {
+      { "<leader>t", "<cmd>ToggleTerm<CR>", desc = "Toggle Terminal" },
+    },
+    cmd = "ToggleTerm",
+    config = function()
+      require("toggleterm").setup({
+        size = 20,
+        open_mapping = [[<leader>t]],
+        hide_numbers = true,
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = 2,
+        start_in_insert = true,
+        insert_mappings = true,
+        persist_size = true,
+        direction = "float",
+        close_on_exit = true,
+        shell = vim.o.shell,
+        float_opts = {
+          border = "curved",
+          winblend = 0,
+          highlights = {
+            border = "Normal",
+            background = "Normal",
+          },
+        },
+      })
+      
+      function _G.set_terminal_keymaps()
+        local opts = { noremap = true }
+        vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+      end
+      
+      vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+    end,
+  },
+  
+  -- Better buffer closing
+  {
+    "famiu/bufdelete.nvim",
+    cmd = { "Bdelete", "Bwipeout" },
+    keys = {
+      { "<leader>bd", "<cmd>Bdelete<CR>", desc = "Delete Buffer" },
+    },
+  },
+}
